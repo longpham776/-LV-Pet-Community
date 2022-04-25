7e35b8363c6896a2eafa043b6210ade1b6498056
Coffeeholic17 <phannhan3025@gmail.com>
2022-04-25 23:13:17 +0700

    Them, xoa, sua loai, hang, admin

--- app/Models/loaisp.php
+++ app/Models/loaisp.php
@@ -11,4 +11,10 @@ class loaisp extends Model
     public static function getLoai(){
         return DB::select('SELECT * FROM loaisp ');
     }
+    public static function addLoai($ten,$tt){
+        return DB::select('INSERT INTO loaisp(tenloai,trangthai) VALUES(?,?)',[$ten,$tt]);
+   }
+   public static function delete_Loai($m){
+       return DB::select('DELETE FROM loaisp WHERE maloai=?',[$m]);
+   }
 }
---
+++ resources/views/backend/thuonghieu.blade.php
@@ -0,0 +1,48 @@
+@extends('backend.masterview')
+@section('content')
+<div class="main-content">
+<section id="main-content">
+	<section class="wrapper">
+		<div class="table-agile-info">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <span class="tools pull-right">
+      <form action="{{route('ad.addhang')}}" method="get">
+      <input name="tenhang" minlength="2" type="text" placeholder="Tên hãng mới" >
+      <button class="btn btn-primary" >Thêm</button>
+      </form>
+    </span>
+    </div>
+    <div class="table-responsive">
+      <table class="table table-striped b-t b-light">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Tên hãng</th>
+            <th>Xóa</th>
+          </tr>
+
+        </thead>
+        <tbody>
+          @foreach($getTH as $th)
+          <form action="{{route('ad.delete_hang')}}" method="get">
+              @csrf
+            <input type="hidden" value="{{ $th->math }}" name="math">
+            <td>{{$th->math}}</td>
+            <td>{{$th->tenth}}</td>
+            <td><button>Xóa</button></td>
+            </form>
+           
+          </tr>
+          
+          <tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+    
+  </div>
+</div>
+</section>
+</div>
+            @stop
